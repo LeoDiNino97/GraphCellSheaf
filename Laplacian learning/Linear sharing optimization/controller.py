90f99594e2021f9d47fead610f27a13e5d535d5b
LeoDiNino97 <dinino.1919479@studenti.uniroma1.it>
2024-05-10 17:08:33 +0200

    Up

--- Laplacian learning/Linear sharing optimization/controller.py
+++ Laplacian learning/Linear sharing optimization/controller.py
@@ -6,52 +6,43 @@ def learning(
         V:int,
         d:int,
         X:np.array,
-        LR:np.array,
         rho:float,
         lambda_:float,
-        gamma:float,
-        T:int,
-        t:int,
         max_iter:int
     ):
     
     # Initialization 
 
-    maps_0 = opt.initialization(V,d)
     edges_dict = opt.edges_map(V)
     edges = list(edges_dict.keys())
     E = len(edges)
 
     # Every local agent has its own buffer for previous iteration
-
     buffer_A = {
-        e: {
-            e[0] : np.eye(d),
-            e[1] : np.eye(d)
-        }
+        e: np.zeros((d*V,d*V))
         for e in edges
     }
-    
+
+    for e in edges:
+        u = e[0]
+        v = e[0]
+
+        buffer_A[e][u*d:(u+1)*d,u*d:(u+1)*d] = np.eye(d)
+        buffer_A[e][v*d:(v+1)*d,v*d:(v+1)*d] = np.eye(d)
+
+        buffer_A[e][u*d:(u+1)*d,v*d:(v+1)*d] = - np.eye(d)
+        buffer_A[e][v*d:(v+1)*d,u*d:(u+1)*d] = - np.eye(d)
+
     # Every local agent has its own buffer for receiving global messages
 
     buffer_B = {
-        e: {
-            'BB_uu': np.eye(d),
-            'BB_uv': np.eye(d),
-            'BB_vv': np.eye(d),
-            'L_uu': np.eye(d),
-            'L_uv': np.eye(d),
-            'L_vv': np.eye(d),
-            'M_uu': np.eye(d),
-            'M_uv': np.eye(d),
-            'M_vv': np.eye(d)
-        }
+        e: (np.eye(d*V),np.eye(d*V))
         for e in edges
     }
 
     # Initialization of the shared multiplier
 
-    M = np.eye(V*d)
+    M = np.eye(d*V)
 
     for _ in tqdm(range(max_iter)):
 
@@ -62,56 +53,30 @@ def learning(
         # Inner loop for the local updates
 
         for e in edges:
-            
+
             u = e[0]
             v = e[1]
 
-            # Data on the nodes of interest
-
-            X_u = X[u*d:(u+1)*d,:]
-            X_v = X[u*d:(u+1)*d,:]
-            
-            # We use the same initialization for both the inner SCA and the outer ADMM
-            
-            F_u_0 = maps_0[e][u]
-            F_v_0 = maps_0[e][u]
-
-            # Retrieve previous iteration from the buffer
-
-            F_u_k = buffer_A[e][u]
-            F_v_k = buffer_A[e][v]
-
-            # Received messages from central aggregator
-
-            L_uu = buffer_B[e]['L_uu']
-            L_uv = buffer_B[e]['L_uv']
-            L_vv = buffer_B[e]['L_vv']
-
-            BB_uu = buffer_B[e]['BB_uu']
-            BB_uv = buffer_B[e]['BB_uv']
-            BB_vv = buffer_B[e]['BB_vv']
-
-            M_uu = buffer_B[e]['M_uu']
-            M_uv = buffer_B[e]['M_uv']
-            M_vv = buffer_B[e]['M_vv']
-
-            # Restriction maps learning
-
-            F_u, F_v = opt.local_proxies(X_u, X_v,
-                                         F_u_0, F_u_k,
-                                         F_v_0, F_v_k,
-                                         L_uu, L_uv, L_vv,
-                                         BB_uu, BB_uv, BB_vv,
-                                         M_uu, M_uv, M_vv,
-                                         rho, gamma, LR, T, t)
+            # Local update
+            BB = buffer_A[e]
+            L_hat = buffer_B[e][1]
+            BB_hat = buffer_B[e][0]
+            BB_e = opt.local_updates(e, d, X, L_hat, BB, BB_hat, M, rho)
 
             # Update local buffer
-            buffer_A[e][u] = F_u
-            buffer_A[e][v] = F_v
+            BB_e_ = np.zeros_like(BB_e)
+
+            BB_e_[u*d:(u+1)*d,u*d:(u+1)*d] = BB_e[u*d:(u+1)*d,u*d:(u+1)*d]
+            BB_e_[v*d:(v+1)*d,v*d:(v+1)*d] = BB_e[v*d:(v+1)*d,v*d:(v+1)*d]
+
+            BB_e_[u*d:(u+1)*d,v*d:(v+1)*d] = BB_e[u*d:(u+1)*d,v*d:(v+1)*d]
+            BB_e_[v*d:(v+1)*d,u*d:(u+1)*d] = BB_e[v*d:(v+1)*d,u*d:(u+1)*d]
+
+            buffer_A[e] = BB_e_
 
             # Send the message to the central aggregator
 
-            BB_ += opt.local_to_global(F_u, F_v, e, d, V, edges_dict)
+            BB_ += BB_e
 
         # Central updates
         BB_ /= E
@@ -121,7 +86,7 @@ def learning(
         # Send the messages from the central aggregator to local agents
 
         for e in edges:
-            buffer_B[e] = opt.global_to_local(d, e, L, BB_, M)
+            buffer_B[e] = (BB_, L)
 
     # We provide as output all the computed restriction maps 
 
--- Laplacian learning/Linear sharing optimization/optimization_utils.py
+++ Laplacian learning/Linear sharing optimization/optimization_utils.py
@@ -18,10 +18,7 @@ def initialization(
 
     all_edges = list(combinations(range(V),2))
     maps = {
-        e:{
-            e[0] : np.eye(d),
-            e[1] : np.eye(d)
-        }
+        e: np.eye(d*V)
         for e in all_edges
     }
 
@@ -54,206 +51,33 @@ def edges_map(
 #________________________________LOCAL UPDATE_______________________________________
 #___________________________________________________________________________________
 
-def local_proxies(
-        X_u:np.array,
-        X_v:np.array,
-        F_u_0:np.array,
-        F_u_k:np.array,
-        F_v_0:np.array,
-        F_v_k:np.array,
-        L_uu:np.array,
-        L_uv:np.array,
-        L_vv:np.array,
-        BB_uu:np.array,
-        BB_uv:np.array,
-        BB_vv:np.array,
-        M_uu:np.array,
-        M_uv:np.array,
-        M_vv:np.array,
+def local_updates(
+        edge:tuple,
+        d:int,
+        X:np.array,
+        L_hat:np.array,
+        BB:np.array,
+        BB_hat:np.array,
+        M:np.array,
         rho:float,
-        LR:float,
-        gamma:float,
-        T:int,
-        t:int
         ) -> tuple:
     
     '''
-    Optimize the local restriction maps through successive convex approximation on 
-    a certain edge calling a subroutine performing a gradient based procedure. 
+    Optimize the local blocks on a certain edge in a closed form (strongly convex problem in each block) 
 
     Parameters:
-    - X_u (np.array): Signals on node u 
-    - X_v (np.array): Signals on node v
-    - F_u_0 (np.array): Initialization for map F_u
-    - F_u_k (np.array): Buffered previous iteration result for F_u
-    - F_v_0 (np.array): Initialization for map F_v
-    - F_v_k (np.array): Buffered previous iteration result for F_u
-    - L_uu (np.array): (u,u) block in the global variable for the sheaf laplacian 
-    - L_uv (np.array): (u,v) block in the global variable for the sheaf laplacian
-    - L_vv (np.array): (v,v) block in the global variable for the sheaf laplacian
-    - BB_uu (np.array): (u,u) block in the global aggregator for local to global messages
-    - BB_uv (np.array): (u,v) block in the global aggregator for local to global messages
-    - BB_vv (np.array): (v,v) block in the global aggregator for local to global messages 
-    - M_uu (np.array): (u,u) block in the shared multiplier 
-    - M_uv (np.array): (u,v) block in the shared multiplier  
-    - M_vv (np.array): (v,v) block in the shared multiplier 
-    - rho (float): coefficient of the augmentation term in the Lagrangian equation of the problem 
-    - LR (float): learning rate for the inner gradient descent subroutine
-    - gamma (float): learning rate for the convex smoothing within the SCA routine
-    - T (int): max number of iterations for the outer routine (SCA)
-    - t (int): max number of iterations for inner routine (gradient based)
+
 
     Returns:
-    - tuple: A tuple containing the two restriction maps on the edge of interest    
+    - np.array: An array containing the block defined on the edge of interest    
     '''
+    u = edge[0]
+    v = edge[1]
+    X_ = np.zeros_like(X)
+    X_[u*d:(u+1)*d,:] = X[u*d:(u+1)*d,:]
+    X_[v*d:(v+1)*d,:] = X[v*d:(v+1)*d,:]
 
-    # External initialization 
-
-    F_u = F_u_0
-    F_v = F_v_0
-
-    # These quantities in the gradients of the block losses are fixed
-
-    l_uu = - F_u_k.T @ F_u_k + BB_uu - L_uu + M_uu
-    l_vv = - F_v_k.T @ F_v_k + BB_vv - L_vv + M_vv
-    l_uv = + F_u_k.T @ F_v_k + BB_uv - L_uv + M_uv
-
-    # Block descent procedure
-
-    for _ in range(T):
-        for _ in range(t):
-            F_u_hat = gradient_U(X_u, X_v, 
-                            F_u, F_v, 
-                            l_uu, l_uv,
-                            rho, LR)
-            
-        for _ in range(t):
-            F_v_hat = gradient_V(X_u, X_v, 
-                            F_u, F_v, 
-                            l_vv, l_uv,
-                            rho, LR)
-
-        F_u += gamma*(F_u_hat - F_u)
-        F_v += gamma*(F_v_hat - F_v)
-
-        gamma *= 0.9
-        
-    return (F_u, F_v)
-
-
-def gradient_U(
-        X_u:np.array,
-        X_v:np.array,
-        F_u:np.array,
-        F_v:np.array,
-        l_uu:np.array,
-        l_uv:np.array,
-        rho:float,
-        LR:float,
-        ) -> np.array:
-    
-    '''
-    Gradient based procedure on block F_u in the local optimization step. 
-
-    Parameters:
-    - X_u (np.array): Signals on node u 
-    - X_v (np.array): Signals on node v
-    - F_u (np.array): Current value for map F_u
-    - F_v (np.array): Current value for map F_v
-    - l_uu (np.array): Fixes term for block (u,u) in the augmentation term;
-    - l_uv (np.array): Fixes term for block (u,v) in the augmentation term;
-    - rho (float): coefficient of the augmentation term in the Lagrangian equation of the problem 
-    - LR (float): learning rate for the inner gradient descent subroutine
-
-    Returns:
-    - np.array: The restriction map F_u  
-    '''    
-
-    l_uu_ = F_u.T @ F_u + l_uu
-    l_uv_ = - F_u.T @ F_v + l_uv
-
-    grad_u = ( (F_u @ X_u - F_v @ X_v) @ X_u.T               # Gradient of the loss on the local communication
-                + rho * (F_u @ l_uu_ - 2*F_v @ l_uv_) )      # Gradient of the block-wise losses
-
-    F_u -= LR * grad_u
-
-    return F_u
-
-def gradient_V(
-        X_u:np.array,
-        X_v:np.array,
-        F_u:np.array,
-        F_v:np.array,
-        l_vv:np.array,
-        l_uv:np.array,
-        rho:float,
-        LR:float,
-        ) -> np.array:
-    
-    '''
-    Gradient based procedure on block F_v in the local optimization step. 
-
-    Parameters:
-    - X_u (np.array): Signals on node u 
-    - X_v (np.array): Signals on node v
-    - F_u (np.array): Current value for map F_u
-    - F_v (np.array): Current value for map F_v
-    - l_vv (np.array): Fixes term for block (v,v) in the augmentation term;
-    - l_uv (np.array): Fixes term for block (u,v) in the augmentation term;
-    - rho (float): coefficient of the augmentation term in the Lagrangian equation of the problem 
-    - LR (float): learning rate for the inner gradient descent subroutine
-
-    Returns:
-    - np.array: The restriction map F_v
-    '''    
-
-    l_vv_ = F_v.T @ F_v + l_vv
-    l_uv_ = - F_u.T @ F_v + l_uv
-
-    grad_v = ( - (F_u @ X_u - F_v @ X_v) @ X_v.T       # Gradient of the loss on the local communication
-                + rho * (F_v @ l_vv_ - 2*F_u @ l_uv_) )  # Gradient of the block-wise losses
-                                                           
-
-    F_v -= LR * grad_v
-
-    return F_v
-
-def local_to_global(
-        F_u:np.array,
-        F_v:np.array,
-        e:tuple,
-        d:int,
-        V:int,
-        edges_dict:dict
-        ) -> np.array:
-    
-    '''
-    Message passing from the local agents to the central aggregator 
-
-    Parameters:
-    - F_u (np.array): Restriction map F_u
-    - F_v (np.array): Restriction map F_v
-    - e (tuple): The edge where (u,v) are incident on
-    - d (int): Dimension of the vertices stalks
-    - V (int): Number of vertices
-    - edges_dict (dict): Dictionary containing the identifiers for the edges
-
-    Returns:
-    - np.array: The local-to-global message
-    '''        
-
-    E = len(list(combinations(range(V),2)))
-    B_e = np.zeros((d*E, d*V))
-
-    u = e[0]
-    v = e[1]
-
-    id = edges_dict[e]
-
-    B_e[id*d:(id+1)*d, u*d:(u+1)*d] = F_u
-    B_e[id*d:(id+1)*d, v*d:(v+1)*d] = - F_v
-
-    return B_e.T @ B_e
+    return 1/rho * (BB + L_hat - BB_hat - M - X_ @ X_.T)
 
 
 #__________________________________________________________________________________________
@@ -315,61 +139,6 @@ def global_update_M(
     '''
 
     return M + BB - L 
-
-def global_to_local(
-        d:int,
-        edge:tuple,
-        L:np.array,
-        BB:np.array,
-        M:np.array
-        ) -> dict:
-    
-    '''
-    Messaging between central aggregator and local agents.
-
-    Parameters:
-    - d (int): stalks dimension
-    - edge (tuple): edge to which the global-to-local is directioned
-    - M (np.array): current estimate of the shared multiplier
-    - BB (np.array): current aggregator of local-to-central messages
-    - L (np.array): current estimate of the global variable for the laplacian
-
-    Returns:
-    - dict: Dictionary containing all the global-to-local messages
-    '''    
-
-    u = edge[0]
-    v = edge[1]
-
-    # Defining the blocks to be sent for the message passing 
-
-    BB_uu = BB[u*d:(u+1)*d,u*d:(u+1)*d]
-    BB_uv = BB[u*d:(u+1)*d,v*d:(v+1)*d]
-    BB_vv = BB[v*d:(v+1)*d,v*d:(v+1)*d]
-
-    L_uu = L[u*d:(u+1)*d,u*d:(u+1)*d]
-    L_uv = L[u*d:(u+1)*d,v*d:(v+1)*d]
-    L_vv = L[v*d:(v+1)*d,v*d:(v+1)*d]
-
-    M_uu = M[u*d:(u+1)*d,u*d:(u+1)*d]
-    M_uv = M[u*d:(u+1)*d,v*d:(v+1)*d]
-    M_vv = M[v*d:(v+1)*d,v*d:(v+1)*d]
-
-    # Mapping the block to be sent
-
-    D = {
-        'BB_uu':BB_uu,
-        'BB_uv':BB_uv,
-        'BB_vv':BB_vv,
-        'L_uu':L_uu,
-        'L_uv':L_uv,
-        'L_vv':L_vv,
-        'M_uu':M_uu,
-        'M_uv':M_uv,
-        'M_vv':M_vv
-    }
-
-    return D
 
 
     
